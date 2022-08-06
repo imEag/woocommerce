e0f75cdea87e898069c7904998fcc20a21bbc17d
Emmanuel <emma145@hotmail.es>
2022-08-05 21:24:08 -0500

    create styles for create component

--- src/components/Create_testimonial.jsx
+++ src/components/Create_testimonial.jsx
@@ -9,7 +9,71 @@ import { CREATE_TESTIMONIAL } from '../queries/createTestimonial';
 import { useMutation } from "@apollo/client";
 
 const StyledCreate = styled.div`
+    background: linear-gradient(180deg, #E5EFFF 0%, rgba(229, 239, 255, 0.262661) 83.7%, rgba(229, 239, 255, 0) 100%);
     
+    & Header {
+        padding: 1.2rem 5.5rem;
+        z-index: 3;
+    }
+
+    & form {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 5rem;
+        padding: 5rem 0;
+
+        & label {
+            font-family: ${props => props.theme.font.secundary};
+            color: ${props => props.theme.color.dark_blue};
+            font-size: ${props => props.theme.font_size.medium_bigger};
+            font-weight: bold;
+        }
+
+        & input {
+            font-family: ${props => props.theme.font.primary};
+            color: ${props => props.theme.color.dark_blue};
+            font-size: ${props => props.theme.font_size.small};
+            padding: 2rem;
+            border-radius: 1rem;
+        }
+
+        & textarea {
+            font-family: ${props => props.theme.font.primary};
+            color: ${props => props.theme.color.dark_blue};
+            font-size: ${props => props.theme.font_size.small};
+            padding: 2rem;
+            border-radius: 1rem;
+        }
+    }
+
+    & h2 {
+        font-family: ${props => props.theme.font.secundary};
+        color: ${props => props.theme.color.dark_blue};
+        font-size: ${props => props.theme.font_size.bigger};
+        text-align: center;
+        margin: 4rem 0;
+
+        @media (max-width: 600px) {
+            font-size: ${props => props.theme.font_size.big};
+        }
+    }
+
+    & div#contentContainer {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 2rem;
+
+        
+    }
+
+    & button#submit {
+        padding: 1rem 3rem;
+        margin: 0 auto;
+    }
 `;
 
 
@@ -44,7 +108,7 @@ export const CreateTestimonial = () => {
                 <form onSubmit={(evt) => handleSubmit(evt)}>
                     <div id="contentContainer">
                         <label htmlFor="content">Content</label>
-                        <input type="text" name="content" value={content} onChange={evt => setContent(evt.target.value)} placeholder="Testimonial content" />
+                        <textarea type="textarea" name="content" value={content} onChange={evt => setContent(evt.target.value)} placeholder="Testimonial content" />
                         {/* <label htmlFor="date">Date (not available)</label>
                         <input disabled={true} type="text" name="date" placeholder="date" /> */}
                     </div>
